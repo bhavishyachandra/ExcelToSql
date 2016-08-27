4c9df9ee5bd07cc827aeeb3a23e63ab538c9a8df
bhavikamineni <bhavikamineni@quickenloans.com>
2016-08-27 19:56:48 -0400

    Initial commit.

---
+++ ExcelToSql/MainPage.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace ExcelToSql
+{
+    partial class MainPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.browserButton = new System.Windows.Forms.Button();
+            this.uploadButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.folderSelectedLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // statusTextBox
+            // 
+            this.statusTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.statusTextBox.Location = new System.Drawing.Point(17, 217);
+            this.statusTextBox.Multiline = true;
+            this.statusTextBox.Name = "statusTextBox";
+            this.statusTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.statusTextBox.Size = new System.Drawing.Size(350, 104);
+            this.statusTextBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(174, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Please select the folder";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 175);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Status:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // browserButton
+            // 
+            this.browserButton.Location = new System.Drawing.Point(17, 124);
+            this.browserButton.Name = "browserButton";
+            this.browserButton.Size = new System.Drawing.Size(75, 28);
+            this.browserButton.TabIndex = 3;
+            this.browserButton.Text = "Browse";
+            this.browserButton.UseVisualStyleBackColor = true;
+            this.browserButton.Click += new System.EventHandler(this.browserButton_Click);
+            // 
+            // uploadButton
+            // 
+            this.uploadButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.uploadButton.Location = new System.Drawing.Point(291, 124);
+            this.uploadButton.Name = "uploadButton";
+            this.uploadButton.Size = new System.Drawing.Size(75, 28);
+            this.uploadButton.TabIndex = 4;
+            this.uploadButton.Text = "Upload";
+            this.uploadButton.UseVisualStyleBackColor = true;
+            this.uploadButton.Click += new System.EventHandler(this.uploadButton_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 50);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(122, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Folder selected:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(141, 50);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 20);
+            this.label4.TabIndex = 6;
+            // 
+            // folderSelectedLabel
+            // 
+            this.folderSelectedLabel.AutoSize = true;
+            this.folderSelectedLabel.Location = new System.Drawing.Point(141, 50);
+            this.folderSelectedLabel.Name = "folderSelectedLabel";
+            this.folderSelectedLabel.Size = new System.Drawing.Size(47, 20);
+            this.folderSelectedLabel.TabIndex = 7;
+            this.folderSelectedLabel.Text = "None";
+            // 
+            // MainPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(378, 344);
+            this.Controls.Add(this.folderSelectedLabel);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.uploadButton);
+            this.Controls.Add(this.browserButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.statusTextBox);
+            this.Name = "MainPage";
+            this.Text = "DataUploader";
+            this.Load += new System.EventHandler(this.MainPage_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox statusTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button browserButton;
+        private System.Windows.Forms.Button uploadButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label folderSelectedLabel;
+    }
+}
+
---
+++ ExcelToSql/MainPage.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExcelToSql
+{
+    public partial class MainPage : Form
+    {
+        private readonly string _connectionString;
+        private string _selectedPath;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            _connectionString = ConfigurationManager.ConnectionStrings["destinationDatabaseConnectionString"].ConnectionString;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void browserButton_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog
+            {
+                RootFolder = Environment.SpecialFolder.Desktop
+            };
+
+            DialogResult result = fbd.ShowDialog();
+
+            _selectedPath = fbd.SelectedPath;
+
+            folderSelectedLabel.Text = _selectedPath;
+        }
+
+        private void StatusUpdate(string message)
+        {
+            statusTextBox.AppendText(DateTime.Now + " : " + message + Environment.NewLine);
+        }
+
+        private string SaveFileToDatabase(string filePath)
+        {
+
+            try
+            {
+                string excelConnectionString =
+                        $"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={filePath};Extended Properties=\"Excel 12.0\"";
+                using (OleDbConnection excelConnection = new OleDbConnection(excelConnectionString))
+                {
+                    excelConnection.Open();
+                    var dtSchema = excelConnection.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, new object[] { null, null, null, "TABLE" });
+                    var sheet1 = dtSchema.Rows[0].Field<string>("TABLE_NAME");
+                    using (OleDbCommand cmd = new OleDbCommand(ConfigurationManager.AppSettings["excelQuery"] + "[" + sheet1 + "]", excelConnection))
+                    {
+                        using (OleDbDataReader dReader = cmd.ExecuteReader())
+                        {
+                            using (SqlBulkCopy sqlBulk = new SqlBulkCopy(_connectionString))
+                            {
+                                sqlBulk.DestinationTableName = ConfigurationManager.AppSettings["destinationTable"];
+                                if (dReader != null) sqlBulk.WriteToServer(dReader);
+                            }
+                        }
+                    }
+                }
+
+                return "Successfully inserted " + Path.GetFileName(filePath);
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        private void uploadButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!string.IsNullOrWhiteSpace(_selectedPath))
+                {
+                    string[] files = Directory.GetFiles(_selectedPath);
+
+                    foreach (string file in files)
+                    {
+                        if (Path.GetExtension(file) == null) continue;
+                        if (Path.GetExtension(file).Equals(".xls") || Path.GetExtension(file).Equals(".xlsx"))
+                        {
+                            StatusUpdate(SaveFileToDatabase(file));
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                this.StatusUpdate(ex.Message);
+            }
+
+        }
+
+        private void MainPage_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
